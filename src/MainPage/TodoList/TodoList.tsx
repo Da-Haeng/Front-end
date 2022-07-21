ba15eaf380e50a99bb7d01edbae35d94ea23a5dd
louisluzet <louisluzet@naver.com>
2022-07-21 23:58:48 +0900

    complete todo create!

--- src/MainPage/TodoList/TodoList.tsx
+++ src/MainPage/TodoList/TodoList.tsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import TodoItem from "./TodoItem";
 import { TodoStateContext } from "./TodoTemplete";
 import "./Todo.css";
 const TodoList = () => {
   const todoData = useContext(TodoStateContext);
-  const data = todoData['todoData'];
+  useEffect(() => {
+    console.log(todoData);
+  }, [])
   return (
     <div className="TodoList">
-        {data && data.map((it) => <div key={it.id}><TodoItem {...it} /></div>)}
+        {todoData && todoData.map((it: any) => <div key={it.id}><TodoItem {...it} /></div>)}
     </div>
   );
 };
--- src/MainPage/TodoList/TodoTemplete.tsx
+++ src/MainPage/TodoList/TodoTemplete.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { useEffect } from "react";
 import { useReducer } from "react";
 import TodoList from "./TodoList";
@@ -11,14 +11,14 @@ export type todos = {
   todo: string;
   done: boolean;
 };
-const reducer = (state: any, action: any) => {
-  let newState: any = [];
+const reducer = (state , action: any) => {
+  let newState = [];
   switch (action.type) {
     case "INIT": {
       return action.data;
     }
     case "CREATE": {
-      newState = [action.data, ...state];
+      newState = [...state, action.data];
       break;
     }
     default:
@@ -28,7 +28,7 @@ const reducer = (state: any, action: any) => {
 };
 
 export const TodoStateContext = React.createContext<[]>([]);
-const TodoTemplete = (todoData) => {
+const TodoTemplete = ({ todoData }) => {
   const [open, setOpen] = useState(true);
   const [todo, setTodo] = useState("");
   const [done, setDone] = useState(false);
@@ -37,15 +37,17 @@ const TodoTemplete = (todoData) => {
   };
 
   const [data, dispatch] = useReducer(reducer, todoData);
+
   useEffect(() => {
     if (data) {
       dispatch({ type: "INIT", data: todoData });
     }
-  }, [todoData]);
+  }, []);
 
   const dataId = useRef(5);
 
-  const onSubmit = () => {
+  const onCreate = (e) => {
+    e.preventDefault();
     dispatch({
       type: "CREATE",
       data: {
@@ -54,6 +56,8 @@ const TodoTemplete = (todoData) => {
         done: false,
       },
     });
+    setTodo("");
+    onToggle();
     dataId.current += 1;
   };
 
@@ -64,7 +68,7 @@ const TodoTemplete = (todoData) => {
         <TodoList />
         <div className="todo_create_form">
           {open && (
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onCreate}>
               <input value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="할 일을 입력 후, Enter를 누르세요"/>
             </form>
           )}
